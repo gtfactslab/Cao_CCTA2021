fixed numpy broadcasting errors, now getting unspecified parameters error

modeling.py
@@ -3,6 +3,8 @@ import numpy as np
 import scipy as sp
 from scipy import sparse
 
+# overall problem formulation is x[k+1] = x[k] + A * f[k] + B
+# where f[k] is a vector [f_1, f_2, ... f_n, f_1r, ..., f_nr]' which represent the outflow from each cell/onramp respectively
 A = sparse.csc_matrix([
   [-1.,      0.,     0., 0., 0., 1],
   [1.,      -1.,     0., 0., 0., 0],
@@ -11,21 +13,23 @@ A = sparse.csc_matrix([
   [0.,      0,      0., 1., -1., 0],
   [0.,      0,      0., 0., 0., -1]])
 
-B = sparse.csc_matrix([
-  [100.],
+# represents inflow that doesn't come from other cells (aka flow from outside the system)
+B = np.array([
   [0.],
   [0.],
   [0.],
   [0.],
-  [150.]])
+  [0.],
+  [150.]]) # in this case, every timestep adds 150 vehicles to the onramp
 [r, c] = A.shape
 
 # reference values for
 # supply
-w_list = np.array([-100, -100, -100, -100, -100])
-x_jam_list = np.array([600, 600, 600, 600, 600])
+w_list = [-100, -100, -100, -100, -100]
+w_matrix = sparse.diags(w_list[1:])
+x_jam_list = np.array([[600], [600], [600], [600], [600]])
 # demand
-v_list = np.array([100, 100, 100, 100, 0])
+v_list = sparse.diags([100, 100, 100, 100, 0])
 
 # modeling horizon
 N = 10
@@ -38,18 +42,24 @@ f = Variable((c, N)) # f represents the flow rate exiting each cell/onramp
 x = Variable((c, N+1)) # x represents the density of each cell/onramp
 
 x_init = Parameter(c)
+f_init = Parameter(c)
 
 objective = 0
-constraints = [x[:,0] == x_init]
+constraints = [x[:,0] == x_init, f[:,0] == f_init]
+# iterate over time steps
 for k in range(N):
     # objective: minimize the sum of all cell/onramp densities over time
     objective += sum(x[:,k])
     # calculating density at each time step using values from previous timestep
-    constraints += [x[:,k+1] == np.eye(c) *x[:, k] + A * f[:, k]]
+    constraints += [x[:,k+1] == x[:, k] + A @ f[:, k] + np.squeeze(B)]
     # constraints for demand & supply
-    constraints += [f[:-1,k] <= np.multiply(x[:,k], v_list[:, np.newaxis]), f[:-2,k] <= np.multiply(x[1:-1,k], w_list[1:, np.newaxis]) + x_jam_list[1:].transpose()]
+    constraints += [f[-1, k] == 100] # last flow, f_1r in this case, has consistent outflow
+    constraints += [f[:-1,k] <= x[:-1,k] @ v_list,
+                    f[:-2,k] <= x[1:-1,k] @ w_matrix + np.squeeze(x_jam_list[1:])]
 prob = Problem(Minimize(objective), constraints)
 
 # Simulate
 prob.solve()
 print(prob.value)
+print(f.value)
+print(x.value)