added discretization factor, added graphing of result, added non-negative constraint, fixed supply calculation

modeling.py
@@ -2,6 +2,7 @@ from cvxpy import *
 import numpy as np
 import scipy as sp
 from scipy import sparse
+import matplotlib.pyplot as plt
 
 # overall problem formulation is x[k+1] = x[k] + A * f[k] + B
 # where f[k] is a vector [f_1, f_2, ... f_n, f_1r, ..., f_nr]' which represent the outflow from each cell/onramp respectively
@@ -30,17 +31,17 @@ w_matrix = sparse.diags(w_list[1:])
 x_jam_list = [600, 600, 600, 600, 600]
 supply_b_list = np.array([[-1 * w_list[i] * x_jam_list[i]] for i in range(0, len(x_jam_list))])
 # demand
-v_matrix = sparse.diags([100, 100, 100, 100, 100])
+v_matrix = sparse.diags([100, 100, 100, 100, 0])
 
 # modeling horizon
-N = 10
+N = 40
 
 # discretization factor
 h = 1/100
+A[:-1, :-1] = A[:-1, :-1] * h
 
 # initial state
 x0 = np.zeros(c)
-f0 = np.zeros(c)
 
 # Define problem
 f = Variable((c, N)) # f represents the flow rate exiting each cell/onramp
@@ -54,17 +55,33 @@ constraints = [x[:,0] == x_init]
 # iterate over time steps
 for k in range(N):
     # objective: minimize the sum of all cell/onramp densities over time
-    #objective += sum(x[:,k] ** 2) # we want to square this value to convert from linear to quadratic
-    objective += sum(x[:, k])
+    objective += sum(x[:,k] ** 2) # we want to square this value to convert from linear to quadratic
     # calculating density at each time step using values from previous timestep
-    constraints += [x[:,k+1] == x[:, k] + h * (A @ f[:, k] + np.squeeze(B))]
+    constraints += [x[:,k+1] == x[:, k] + A @ f[:, k] + np.squeeze(B)]
     # constraints for demand & supply
     constraints += [f[-1, k] == 100] # last flow, f_1r in this case, has consistent outflow
     constraints += [f[:-1,k] <= x[:-1,k] @ v_matrix,
                     f[:-2,k] <= x[1:-1,k] @ w_matrix + np.squeeze(supply_b_list[1:])]
+# impose non-negative constraint on x and flow, as a check
+constraints += [x >= 0, f >= 0]
+
 prob = Problem(Minimize(objective), constraints)
 
 prob.solve(verbose=True)
 print(prob.value)
 print(f.value)
 print(x.value)
+
+# plot cell density per time step
+fig, ax = plt.subplots()
+cax = ax.matshow(np.flipud(x.value[:-1, :]), aspect="auto")
+fig.colorbar(cax)
+plt.xlabel("Time Step")
+plt.ylabel("Cell")
+plt.title("Cell Density")
+
+cell_ticks = ['']
+[cell_ticks.append(str(c -1 - i)) for i in range(0, c-1)]
+ax.set_yticklabels(cell_ticks)
+ax.tick_params(axis='x', bottom=True, top=False, labelbottom=True, labeltop=False)
+plt.show()