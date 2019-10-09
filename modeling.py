6237e93fde89bde06ff7e5ff084d859ec8f17dea
Michael E. Cao <mcao34@gatech.edu>
2019-10-09 16:28:06 -0400

    added graphs for supply, demand, and outflow for debugging

--- modeling.py
+++ modeling.py
@@ -16,13 +16,21 @@ A = sparse.csc_matrix([
   [0.,      0,      0., 0., 0., -1]])
 
 # represents inflow that doesn't come from other cells (aka flow from outside the system)
-B = np.array([
+B_1 = np.array([
   [0.],
   [0.],
   [0.],
   [0.],
   [0.],
   [150.]]) # in this case, every timestep adds 150 vehicles to the onramp
+
+B_2 = np.array([
+  [0.],
+  [0.],
+  [0.],
+  [0.],
+  [0.],
+  [0.]]) # after a certain point, we want much fewer vehicles to enter
 [r, c] = A.shape
 
 # reference values for
@@ -32,10 +40,11 @@ w_matrix = sparse.diags(w_list[1:])
 x_jam_list = [600, 600, 600, 600, 600]
 supply_b_list = np.array([[-1 * w_list[i] * x_jam_list[i]] for i in range(0, len(x_jam_list))])
 # demand
-v_matrix = sparse.diags([100, 100, 100, 100, 10])
+v_list = [100, 100, 100, 100, 10]
+v_matrix = sparse.diags(v_list)
 
 # modeling horizon
-N = 41
+N = 61
 
 # discretization factor
 h = 1/100
@@ -56,12 +65,15 @@ constraints = [x[:,0] == x_init]
 # iterate over time steps
 for k in range(N):
     # objective: minimize the sum of all cell/onramp densities over time
-    objective += sum(x[:,k] ** 2) # we want to square this value to convert from linear to quadratic
-    #objective += sum(f[-2, k]) #EXPERIMENT: see what happens if we try to maximize flow instead of minimize density
+    objective += sum(x[:-1,k]) # we want to square this value to convert from linear to quadratic
+
     # calculating density at each time step using values from previous timestep
-    constraints += [x[:,k+1] == x[:, k] + A @ f[:, k] + np.squeeze(B)]
+    if k < 41:
+        constraints += [x[:,k+1] == x[:, k] + A @ f[:, k] + np.squeeze(B_1)]
+    else:
+        constraints += [x[:, k + 1] == x[:, k] + A @ f[:, k] + np.squeeze(B_2)]
     # constraints for demand & supply
-    constraints += [f[-1, k] == 100] # last flow, f_1r in this case, has consistent outflow
+    constraints += [f[-1, k] == 100] # last flow, f_1r in this case, has consistent outflow (aka no control)
     constraints += [f[:-1,k] <= x[:-1,k] @ v_matrix,
                     f[:-2,k] <= x[1:-1,k] @ w_matrix + np.squeeze(supply_b_list[1:])]
 # impose non-negative constraint on x and flow, as a check
@@ -74,6 +86,39 @@ prob.solve(verbose=True, solver=GUROBI)
 print(prob.value)
 print(f.value)
 #print(x.value)
+
+#FOR DEBUGGING
+desired_cell = 0
+for desired_cell in range(5):
+    supplys = []
+    demands = []
+    flows = []
+    times = []
+    for t in range(N):
+        times.append(t)
+
+
+        demand = x.value[desired_cell, t] * v_list[desired_cell]
+        demands.append(demand)
+
+        if desired_cell < r - 2:
+            supply = x.value[desired_cell+1, t] * w_list[desired_cell+1] + supply_b_list[desired_cell+1]
+        else:
+            supply = demand
+        supplys.append(supply)
+
+        flows.append(f.value[desired_cell, t])
+
+
+    fig, ax = plt.subplots()
+    ax.plot(times, supplys, linestyle="dotted")
+    ax.plot(times, demands, linestyle="dashed")
+    ax.plot(times, flows)
+    plt.xlabel("Time Step")
+    plt.ylabel("Value")
+    plt.title("Cell {} Flow vs. Supply/Demand".format(desired_cell + 1))
+    ax.legend(['next cell supply', 'demand', 'outflow'])
+
 
 # plot cell density per time step
 fig, ax = plt.subplots()
