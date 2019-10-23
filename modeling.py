1ba05a946a97a47afa280945ed20cc71cbdeca1f
Michael E. Cao <mcao34@gatech.edu>
2019-10-23 14:54:19 -0400

    fixed how betas are treated, tweaked params, changed f & x to non-int

--- modeling.py
+++ modeling.py
@@ -5,14 +5,19 @@ from scipy import sparse
 import matplotlib.pyplot as plt
 import gurobipy
 
+
 # overall problem formulation is x[k+1] = x[k] + A * f[k] + B
+
+#represents ratio of cars that actually progress to next cell
+betas = [0.9, 0.9, 0.9, 0.9, 1]
+
 # where f[k] is a vector [f_1, f_2, ... f_n, f_1r, ..., f_nr]' which represent the outflow from each cell/onramp respectively
 A = sparse.csc_matrix([
   [-1.,      0.,     0., 0., 0., 1],
-  [1.,      -1.,     0., 0., 0., 0],
-  [0.,      1.,     -1., 0., 0., 0],
-  [0.,       0.,    1., -1., 0., 0],
-  [0.,      0,      0., 1., -1., 0],
+  [betas[0],      -1.,     0., 0., 0., 0],
+  [0.,      betas[1],     -1., 0., 0., 0],
+  [0.,       0.,    betas[2], -1., 0., 0],
+  [0.,      0,      0., betas[3], -1., 0],
   [0.,      0,      0., 0., 0., -1]])
 
 # represents inflow that doesn't come from other cells (aka flow from outside the system)
@@ -22,7 +27,7 @@ B_1 = np.array([
   [0.],
   [0.],
   [0.],
-  [150.]]) # in this case, every timestep adds 150 vehicles to the onramp
+  [40.]]) # in this case, every timestep adds 150 vehicles to the onramp
 
 B_2 = np.array([
   [0.],
@@ -33,32 +38,31 @@ B_2 = np.array([
   [0.]]) # after a certain point, we want much fewer vehicles to enter
 [r, c] = A.shape
 
-#represents ratio of cars that actually progress to next cell
-betas = sparse.diags([0.5, 0.5, 0.5, 0.5, 0.5, 1])
+
 
 # reference values for
 # supply
-w_list = [-200, -200, -200, -200, -200]
+w_list = [-20, -20, -20, -20, -20] # -20
 w_matrix = sparse.diags(w_list[1:])
-x_jam_list = [600, 600, 600, 600, 600]
+x_jam_list = [320, 320, 320, 320, 320] # 320
 supply_b_list = np.array([[-1 * w_list[i] * x_jam_list[i]] for i in range(0, len(x_jam_list))])
 # demand
-v_list = [100, 100, 100, 100, 10]
+v_list = [60, 60, 60, 60, 5] # 60
 v_matrix = sparse.diags(v_list)
 
 # modeling horizon
-N = 41
+N = 51
 
 # discretization factor
-h = 1/100
+h = 1/120
 A[:-1, :-1] = A[:-1, :-1] * h
 
 # initial state
 x0 = np.zeros(c)
 
 # Define problem
-f = Variable((c, N), integer=True) # f represents the flow rate exiting each cell/onramp
-x = Variable((c, N+1), integer=True) # x represents the density of each cell/onramp
+f = Variable((c, N), integer=False) # f represents the flow rate exiting each cell/onramp
+x = Variable((c, N+1),  integer=False) # x represents the density of each cell/onramp
 
 x_init = Parameter(c)
 x_init.value = x0
@@ -68,15 +72,18 @@ constraints = [x[:,0] == x_init]
 # iterate over time steps
 for k in range(N):
     # objective: minimize the sum of all cell/onramp densities over time
-    objective += sum(x[:-1,k])
+    objective += sum(x[:,k])
 
     # calculating density at each time step using values from previous timestep
     if k < 41:
-        constraints += [x[:,k+1] == x[:, k] + betas @ A @ f[:, k] + np.squeeze(B_1)]
+        constraints += [x[:,k+1] == x[:, k] + A @ f[:, k] + np.squeeze(B_1)]
     else:
-        constraints += [x[:, k + 1] == x[:, k] + betas @ A @ f[:, k] + np.squeeze(B_2)]
+        constraints += [x[:, k + 1] == x[:, k] + A @ f[:, k] + np.squeeze(B_2)]
     # constraints for demand & supply
-    constraints += [f[-1, k] == 100] # last flow, f_1r in this case, has consistent outflow (aka no control)
+    if k == 0:
+        constraints += [f[-1, k] == 0] # first time step has no outflow as there are no cars in the system
+    else:
+        constraints += [f[-1, k] == 30] # last flow, f_1r in this case, has consistent outflow (aka no control)
     constraints += [f[:-1,k] <= x[:-1,k] @ v_matrix,
                     f[:-2,k] <= x[1:-1,k] @ w_matrix + np.squeeze(supply_b_list[1:])]
     # for i in range(r - 1):
@@ -109,15 +116,15 @@ for desired_cell in range(5):
 
 
         demand = x.value[desired_cell, t] * v_list[desired_cell]
-        demands.append(demand)
+        demands.append(demand* h)
 
         if desired_cell < r - 2:
             supply = x.value[desired_cell+1, t] * w_list[desired_cell+1] + supply_b_list[desired_cell+1]
         else:
             supply = demand
-        supplys.append(supply)
+        supplys.append(supply * h)
 
-        flows.append(f.value[desired_cell, t])
+        flows.append(f.value[desired_cell, t] * h)
 
 
     fig, ax = plt.subplots()
@@ -125,7 +132,7 @@ for desired_cell in range(5):
     ax.plot(times, demands, linestyle="dashed")
     ax.plot(times, flows)
     plt.xlabel("Time Step")
-    plt.ylabel("Value")
+    plt.ylabel("Value (# of cars)")
     plt.title("Cell {} Flow vs. Supply/Demand".format(desired_cell + 1))
     ax.legend(['next cell supply', 'demand', 'outflow'])
 
